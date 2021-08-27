dfd6f0ec6f61a69b37521602d5797850caa3eb98
Julian de Almeida Santos <julian.santos.info@gmail.com>
2021-08-27 17:52:43 -0300

    Added docker settings file.

---
+++ core/settings/docker.py
@@ -0,0 +1,27 @@
+import environ
+from core.settings.settings import *
+
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False),
+    SECRET_KEY=(str, '{{secret_key}}'),
+    ALLOWED_HOSTS=(list, "'*'"),
+)
+
+DEBUG = env('DEBUG')
+
+SECRET_KEY = env('SECRET_KEY')
+
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': '5432'
+    }
+}
