3ec0a4ed7a6bf005d7e77dd3c6c5d38b00d1c0a3
Julian Santos <julian.santos.info@gmail.com>
2022-09-03 09:13:49 -0300

    Add folder to db.sqlite files and backups. Change settings default databases.

--- core/settings/settings.py
+++ core/settings/settings.py
@@ -89,7 +89,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'databases/default.db.sqlite3',
+    },
+    'development': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'databases/development.db.sqlite3',
     }
 }
 
